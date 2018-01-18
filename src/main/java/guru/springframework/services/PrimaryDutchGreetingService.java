a2605e249090020d558c992ed91518a4b8ea7995
Michael Kalisvaart <mkalisvaart@bol.com>
2018-01-18 09:38:01 +0100

    dutch profile switch primary bean

--- src/main/java/guru/springframework/services/PrimaryDutchGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryDutchGreetingService.java
@@ -1,4 +1,15 @@
 package guru.springframework.services;
 
-public class PrimaryDutchGreetingService {
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("nl")
+@Primary
+public class PrimaryDutchGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Dit is de primary greetings bean";
+    }
 }
--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,9 +1,11 @@
 package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("en")
 @Primary
 public class PrimaryGreetingService implements GreetingService {
     @Override
